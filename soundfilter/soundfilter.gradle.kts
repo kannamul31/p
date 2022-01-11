4ed1335cd28294d5ddd2b1b6adf5e9860bf71472
kannamul31 <97486835+kannamul31@users.noreply.github.com>
2022-01-10 20:59:00 -0500

    Add files via upload

---
+++ soundfilter/soundfilter.gradle.kts
@@ -0,0 +1,19 @@
+version = "1.0.1"
+
+project.extra["PluginName"] = "Sound Filter"
+project.extra["PluginDescription"] = "Filter ingame sounds using a white/blacklist"
+project.extra["ProjectSupportUrl"] = "https://github.com/Sundar-Gandu/Pajeet-Plugins"
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                    "Plugin-Version" to project.version,
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                    "Plugin-Provider" to project.extra["PluginProvider"],
+                    "Plugin-Description" to project.extra["PluginDescription"],
+                    "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
+}
